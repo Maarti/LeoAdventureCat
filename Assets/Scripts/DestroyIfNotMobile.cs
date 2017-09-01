1a0d36cce07360c46fe976beef454537c01ee50d
Maarti <bryan.martinet@gmail.com>
2017-09-01 23:56:43 +0200

    arrows on mobile screen + falling animation

---
+++ Assets/Scripts/DestroyIfNotMobile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyIfNotMobile : MonoBehaviour
+{
+
+	#if !UNITY_ANDROID && !UNITY_IPHONE && !UNITY_BLACKBERRY && !UNITY_WINRT || UNITY_EDITOR
+	void Start ()
+	{
+		Destroy (this.gameObject);
+	}
+	#endif
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # LeoAdventureCat
+
+
+## Resources
+* [How to animate a cat walking cycle](https://www.youtube.com/watch?v=dYCGMdQgs-I)
+
+## Author
+[Bryan MARTINET](https://maarti.net)s
