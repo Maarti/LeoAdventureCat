46a8572576bd89ec038de31f8467028f6c94446d
Maarti <bryan.martinet@gmail.com>
2017-09-03 20:58:31 +0200

    background, paralax, fog,...

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		GameObject obj = other.gameObject;
+		if (obj.tag == "Player")
+			obj.transform.position = new Vector3 (0.0f, 0.0f, 0.0f);
+		else
+			Destroy (other.gameObject);
+	}
+
+}
