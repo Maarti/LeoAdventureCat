7e1d773226212f52b537991cb1fb9d40fddec92a
Maarti <bryan.martinet@gmail.com>
2017-09-12 23:57:02 +0200

    new small platform, map redesigned

--- Assets/Scripts/Environment/DestructibleBlocController.cs
+++ Assets/Scripts/Environment/DestructibleBlocController.cs
@@ -17,6 +17,7 @@ public class DestructibleBlocController : MonoBehaviour, IDefendable
 	public void Defend (GameObject attacker, float damage, Vector2 bumpVelocity, float bumpTime)
 	//public void Hit (float damage)
 	{
+		Debug.Log ("defend " + damage);
 		animator.SetTrigger ("hit");
 		life -= damage;
 		if (life <= 0) {
