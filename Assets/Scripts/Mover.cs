bfcdb6a76689cb584a7cf95cfc5f617885b3da29
Maarti <bryan.martinet@gmail.com>
2017-09-15 00:45:34 +0200

    main menu rework

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+	public Vector3 move;
+	public float speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = new Vector3 (
+			Mathf.Clamp (transform.position.x + move.x * speed * Time.deltaTime, -100000, 100000),
+			Mathf.Clamp (transform.position.y + move.y * speed * Time.deltaTime, -100000, 100000),
+			Mathf.Clamp (transform.position.z + move.z * speed * Time.deltaTime, -100000, 100000)
+		);
+			
+	}
+}
