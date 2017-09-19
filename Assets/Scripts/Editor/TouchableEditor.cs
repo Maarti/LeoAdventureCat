21316f13e4a7d5d4af8af4ef61f104141a22aad9
Maarti <bryan.martinet@gmail.com>
2017-09-20 01:20:36 +0200

    can press button outside of img area

---
+++ Assets/Scripts/Editor/TouchableEditor.cs
@@ -0,0 +1,13 @@
+﻿//http://answers.unity3d.com/questions/801928/46-ui-making-a-button-transparent.html?childToView=851816#answer-851816
+// Touchable_Editor component, to prevent treating the component as a Text object.
+
+using UnityEditor;
+
+[CustomEditor (typeof(Touchable))]
+public class Touchable_Editor : Editor
+{
+	public override void OnInspectorGUI ()
+	{
+		// Do nothing
+	}
+}
---
+++ Assets/Scripts/UI/Touchable.cs
@@ -0,0 +1,13 @@
+﻿//http://answers.unity3d.com/questions/801928/46-ui-making-a-button-transparent.html?childToView=851816#answer-851816
+// Touchable component
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Touchable : Text
+{
+	protected override void Awake ()
+	{
+		base.Awake ();
+	}
+}
+
