4306f400afc81ce65ad09fc5ab9fcdadb4c95809
Maarti <bryan.martinet@gmail.com>
2017-09-24 00:50:34 +0200

    tutorial halo on buttons

---
+++ Assets/Scripts/UI/TutorialUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialUI : MonoBehaviour
+{
+
+	public GameObject uiElementOn, uiElementOff;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			SetHalo ();
+			Destroy (this.gameObject);
+		}
+	}
+
+	void SetHalo ()
+	{
+		if (uiElementOn)
+			uiElementOn.GetComponent<Animator> ().SetBool ("halo", true);
+		if (uiElementOff)
+			uiElementOff.GetComponent<Animator> ().SetBool ("halo", false);
+	}
+
+}
