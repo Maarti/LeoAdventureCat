97bcc9ebfb3a50ebd54d6cd2698377c4e2f07097
Maarti <bryan.martinet@gmail.com>
2017-09-23 16:10:00 +0200

    story map created + Tiled folder

---
+++ Tiled/Tiles/1_Forest/forest_tileset.tsx
@@ -0,0 +1,119 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="forest" tilewidth="128" tileheight="128" tilecount="23" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="17">
+  <image width="128" height="128" source="2bis_left.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="3bis_right.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="4bis_mid.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="9_left.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <image width="128" height="128" source="10_right.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <image width="128" height="128" source="11_right.png"/>
+  <objectgroup draworder="index"/>
+ </tile>
+ <tile id="23">
+  <image width="128" height="128" source="12_left.png"/>
+ </tile>
+ <tile id="24">
+  <image width="128" height="128" source="13_center.png"/>
+ </tile>
+ <tile id="25">
+  <image width="128" height="128" source="14_center.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <image width="128" height="128" source="15_left.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <image width="128" height="128" source="16_right.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <image width="128" height="128" source="17_left.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="84"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <image width="128" height="128" source="18_mid.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="84"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <image width="128" height="128" source="19_right.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="84"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <image width="128" height="128" source="20_mid.png"/>
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="128" height="50"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <image width="128" height="128" source="21_right.png"/>
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="42" height="50"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <image width="128" height="128" source="22_left.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="86" y="0" width="42" height="50"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <image width="128" height="128" source="A.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <image width="128" height="128" source="B.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <image width="128" height="128" source="C.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <image width="128" height="128" source="D.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <image width="128" height="128" source="E.png"/>
+ </tile>
+ <tile id="39">
+  <image width="128" height="128" source="Fbis.png"/>
+ </tile>
+</tileset>
