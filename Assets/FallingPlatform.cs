398d43eefb2c5cc98e16a1f8842b31a7367d2b62
Maarti <bryan.martinet@gmail.com>
2017-09-27 13:44:27 +0200

    falling platform created

---
+++ Assets/FallingPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+
+	public float timeBeforeFall = 1f;
+
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		if (other.transform.tag == "Player") {
+			transform.parent.GetComponent<Animator> ().SetTrigger ("collision");
+			Invoke ("Fall", timeBeforeFall);
+		}
+	}
+
+	void Fall ()
+	{
+		transform.parent.GetComponent<Animator> ().enabled = false;
+		GetComponent<Rigidbody2D> ().isKinematic = false;
+	}
+}
