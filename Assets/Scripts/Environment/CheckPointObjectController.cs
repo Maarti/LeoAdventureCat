efc71e416bda0a322259dcc3fb271f4e42b55f47
Maarti <bryan.martinet@gmail.com>
2017-10-05 22:21:19 +0200

    checkpoints designed

--- Assets/Scripts/Environment/CheckPointObjectController.cs
+++ Assets/Scripts/Environment/CheckPointObjectController.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class CheckPointObjectController : MonoBehaviour
 {
+	bool isActivated = false;
+	Animator animator;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		animator = GetComponent<Animator> ();
 	}
 
 	void OnTriggerEnter2D (Collider2D other)
 	{
-		if (other.gameObject.tag == "Player") {
+		if (!isActivated && other.gameObject.tag == "Player") {
 			CheckPointController.cc.Check (this.gameObject.name);
+			isActivated = true;
+			animator.SetTrigger ("checked");
 		}
 	}
 }
