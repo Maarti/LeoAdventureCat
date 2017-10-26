183c0d4ab3a1e4a030afcf16912383fce3c5590f
Maarti <bryan.martinet@gmail.com>
2017-10-26 23:48:57 +0200

    world 2 test

---
+++ Tiled/Tiles/2_Animal_pound/animal_pound_tileset.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset_animal_pound" tilewidth="128" tileheight="128" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="ground1.png"/>
+  <objectgroup draworder="index">
+   <object id="1" name="Ground" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+</tileset>
