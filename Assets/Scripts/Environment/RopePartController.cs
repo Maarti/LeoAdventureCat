80c437f541899efee4d73ec4881fa48495255aa3
Maarti <bryan.martinet@gmail.com>
2017-10-28 20:35:11 +0200

    rope damaging animation

---
+++ Assets/Scripts/Environment/RopePartController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopePartController : MonoBehaviour, IDefendable
+{
+	public int life = 10;
+	Animator animator;
+
+	void Awake ()
+	{
+		animator = GetComponent<Animator> ();
+	}
+
+	public void Defend (GameObject attacker, int damage, Vector2 bumpVelocity, float bumpTime)
+	{
+		animator.SetTrigger ("hit");
+		life -= damage;
+		if (life <= 0)
+			Destroy (this.gameObject);
+		else
+			animator.SetInteger ("life", life);
+	}
+}
