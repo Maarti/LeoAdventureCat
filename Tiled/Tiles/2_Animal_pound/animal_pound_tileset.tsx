ef7487a7de348360b94de3dd9d219e09576eeecb
Maarti <bryan.martinet@gmail.com>
2017-11-02 22:12:11 +0100

    wall designed + window

--- Tiled/Tiles/2_Animal_pound/animal_pound_tileset.tsx
+++ Tiled/Tiles/2_Animal_pound/animal_pound_tileset.tsx
@@ -1,10 +1,25 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset_animal_pound" tilewidth="128" tileheight="128" tilecount="1" columns="0">
+<tileset name="tileset_animal_pound" tilewidth="128" tileheight="128" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="128" height="128" source="ground1.png"/>
   <objectgroup draworder="index">
    <object id="1" name="Ground" x="0" y="0" width="128" height="128"/>
   </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="wall1.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="window_1.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="window_2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="window_3.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="window_4.png"/>
  </tile>
 </tileset>
