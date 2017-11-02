f024f8625123a36dfb508f761c351f2e7727083c
Maarti <bryan.martinet@gmail.com>
2017-11-02 22:53:23 +0100

    shelf implemented

--- Tiled/Tiles/2_Animal_pound/animal_pound_tileset.tsx
+++ Tiled/Tiles/2_Animal_pound/animal_pound_tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset_animal_pound" tilewidth="128" tileheight="128" tilecount="6" columns="0">
+<tileset name="tileset_animal_pound" tilewidth="128" tileheight="128" tilecount="8" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="128" height="128" source="ground1.png"/>
@@ -21,5 +21,17 @@
  </tile>
  <tile id="9">
   <image width="128" height="128" source="window_4.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="shelf_1.png"/>
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="5" y="8" width="123" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="shelf_3.png"/>
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="0" y="8" width="123" height="7"/>
+  </objectgroup>
  </tile>
 </tileset>
