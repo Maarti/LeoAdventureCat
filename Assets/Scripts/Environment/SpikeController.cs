746777a605f1eaabdc73284da107815895a2828f
Maarti <bryan.martinet@gmail.com>
2017-11-11 16:31:32 +0100

    spikes implemented

---
+++ Assets/Scripts/Environment/SpikeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeController : MonoBehaviour
+{
+	public int damageOnCollision = 1;
+	public Vector2 bumpOnCollision = new Vector2 (-2, 1);
+
+	void OnCollisionStay2D (Collision2D other)
+	{
+		if (other.transform.tag == "Player")
+			other.gameObject.GetComponent<PlayerController> ().Defend (this.gameObject, damageOnCollision, bumpOnCollision, 0.5f);
+	}
+}
