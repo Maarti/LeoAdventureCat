b0e135fa58471cb88154d770636b38178a39aa56
Maarti <bryan.martinet@gmail.com>
2017-11-11 13:59:58 +0100

    testing new ground design

--- Tiled/Tiles/2_Animal_pound/animal_pound_tileset.tsx
+++ Tiled/Tiles/2_Animal_pound/animal_pound_tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset_animal_pound" tilewidth="128" tileheight="128" tilecount="8" columns="0">
+<tileset name="tileset_animal_pound" tilewidth="128" tileheight="128" tilecount="12" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="128" height="128" source="ground1.png"/>
@@ -32,6 +32,30 @@
   <image width="128" height="128" source="shelf_3.png"/>
   <objectgroup draworder="index">
    <object id="1" type="Ground" x="0" y="8" width="123" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="ground2.png"/>
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="ground2_H.png"/>
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="ground2_V.png"/>
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="ground2_HV.png"/>
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="0" y="0" width="128" height="128"/>
   </objectgroup>
  </tile>
 </tileset>
