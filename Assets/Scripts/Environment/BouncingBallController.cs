4971d813593cbfc04c2473ceb5d14af6b8ea40c3
Maarti <bryan.martinet@gmail.com>
2017-11-20 23:30:50 +0100

    level 2-story continued + dog ball deceleration script

--- Assets/Scripts/Environment/BouncingBallController.cs
+++ Assets/Scripts/Environment/BouncingBallController.cs
@@ -6,14 +6,26 @@ public class BouncingBallController : MonoBehaviour, IDefendable
 {
 
 	public Vector2 bumpVelocity = new Vector2 (0, 4);
+    public float xDecelaration = 1f;
 
 	Rigidbody2D rb;
+    Vector2 velo;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
 	{
 		rb = GetComponent<Rigidbody2D> ();
 	}
+
+    void Update()
+    {
+        velo = rb.velocity;
+        if (velo.x > 0)
+            velo.x = Mathf.Clamp(velo.x - (xDecelaration * Time.deltaTime), 0f, 5f);
+        else
+            velo.x = Mathf.Clamp(velo.x + (xDecelaration * Time.deltaTime), -5f, 0f);
+        rb.velocity = velo;
+    }
 
 	public void Defend (GameObject attacker, int damage, Vector2 attackerBumpVelocity, float bumpTime)
 	{
