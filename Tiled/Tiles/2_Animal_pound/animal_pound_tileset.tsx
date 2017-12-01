5d306d81928be449744f4d12d98ae7b8404f4168
Maarti <bryan.martinet@gmail.com>
2017-12-01 20:10:54 +0100

    ventilation pipes tiles designed

--- Tiled/Tiles/2_Animal_pound/animal_pound_tileset.tsx
+++ Tiled/Tiles/2_Animal_pound/animal_pound_tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset_animal_pound" tilewidth="128" tileheight="128" tilecount="11" columns="0">
+<tileset name="tileset_animal_pound" tilewidth="128" tileheight="128" tilecount="23" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="1">
   <image width="128" height="128" source="wall1.png"/>
@@ -50,6 +50,86 @@
   <image width="128" height="128" source="ground3_D.png"/>
   <objectgroup draworder="index">
    <object id="1" type="Ground" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <image width="128" height="128" source="pipe_hb.png"/>
+  <objectgroup draworder="index">
+   <object id="2" type="Ground" x="0" y="116" width="128" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <image width="128" height="128" source="pipe_ht.png"/>
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="0" y="0" width="128" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <image width="128" height="128" source="pipe_vl.png"/>
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="0" y="0" width="12" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <image width="128" height="128" source="pipe_vr.png"/>
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="116" y="0" width="12" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <image width="128" height="128" source="pipe_corner_bl.png"/>
+  <objectgroup draworder="index">
+   <object id="2" type="Ground" x="0" y="116" width="12" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <image width="128" height="128" source="pipe_corner_br.png"/>
+  <objectgroup draworder="index">
+   <object id="3" type="Ground" x="116" y="116" width="12" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="41">
+  <image width="128" height="128" source="pipe_corner_tl.png"/>
+  <objectgroup draworder="index">
+   <object id="4" type="Ground" x="0" y="0" width="12" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <image width="128" height="128" source="pipe_corner_tr.png"/>
+  <objectgroup draworder="index">
+   <object id="3" type="Ground" x="116" y="0" width="12" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="43">
+  <image width="128" height="128" source="pipe_round_bl.png"/>
+  <objectgroup draworder="index">
+   <object id="3" type="Ground" x="10.3333" y="0">
+    <polyline points="1.47656,0.214844 4.54167,33.9167 24.6667,71.3333 54.6667,97.6667 91.4848,113.758 117.606,116.424 117.667,128 -10.3333,128 -10.3333,0 1.50599,0.0924479"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="44">
+  <image width="128" height="128" source="pipe_round_br.png"/>
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="116" y="-0.333333">
+    <polyline points="0.03125,0.4375 -1.66667,25 -13.3333,55.6667 -34,82.3333 -60.6667,101.333 -95,114.333 -115.667,116.333 -116,128.333 12,128.333 12,0.333333 0,0.333333"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <image width="128" height="128" source="pipe_round_tl.png"/>
+  <objectgroup draworder="index">
+   <object id="2" type="Ground" x="127.667" y="10.6667">
+    <polyline points="0.34375,-0.03125 -34,6 -64,20.6667 -85.3333,37.6667 -102,61.6667 -112.333,86.6667 -116.333,106.333 -116.333,117 -127.667,117.333 -127.667,-10.6667 0.333333,-10.6667 0.270833,0.0208333"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <image width="128" height="128" source="pipe_round_tr.png"/>
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="0" y="11">
+    <polyline points="0.0222222,0.311111 27,4 58,16.6667 84.3333,37 103.667,63 114.333,91.3333 116.067,116.978 128,117 128,-11 0,-11 0,0.3125"/>
+   </object>
   </objectgroup>
  </tile>
 </tileset>
