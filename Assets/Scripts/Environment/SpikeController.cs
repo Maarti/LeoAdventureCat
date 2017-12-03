fb1fcb3b9a754ad307d1fd71ed3a6a9a3130af78
Maarti <bryan.martinet@gmail.com>
2017-12-03 14:36:23 +0100

    rat spikes : damages the cat when rat hits spikes

--- Assets/Scripts/Environment/SpikeController.cs
+++ Assets/Scripts/Environment/SpikeController.cs
@@ -6,10 +6,14 @@ public class SpikeController : MonoBehaviour
 {
 	public int damageOnCollision = 1;
 	public Vector2 bumpOnCollision = new Vector2 (-2, 1);
+    public bool collidWithRat = false;
 
-	void OnCollisionStay2D (Collision2D other)
+    void OnCollisionStay2D (Collision2D other)
 	{
 		if (other.transform.tag == "Player")
 			other.gameObject.GetComponent<PlayerController> ().Defend (this.gameObject, damageOnCollision, bumpOnCollision, 0.5f);
-	}
+
+        else if (collidWithRat && other.transform.tag == "Rat")
+            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().Defend(this.gameObject, damageOnCollision, Vector2.zero, 0f);
+    }
 }
