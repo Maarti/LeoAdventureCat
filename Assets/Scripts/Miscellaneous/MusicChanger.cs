ad31a94ac1ccd05a1538d07ab9446bab09ca286a
Maarti <bryan.martinet@gmail.com>
2017-12-17 21:13:34 +0100

    music changer created

---
+++ Assets/Scripts/Miscellaneous/MusicChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MusicChanger : MonoBehaviour {
+
+    public AudioClip newAudio;
+    GameObject gameCtrlr;
+
+    void Start () {
+        gameCtrlr = GameObject.Find("GameController");
+	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.transform.tag == "Player") {
+            AudioSource audioSource = gameCtrlr.GetComponent<AudioSource>();
+            audioSource.Stop();
+            audioSource.clip = newAudio;
+            audioSource.Play();
+            Destroy(this.gameObject);
+        }
+    }
+
+}
