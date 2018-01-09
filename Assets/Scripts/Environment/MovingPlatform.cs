8c59b0d3a71a1e01c29c2359d2cda535e52fb3d1
Maarti <bryan.martinet@gmail.com>
2018-01-09 20:21:11 +0100

    smooth moving platform + adjustments to make some levels easier (sorry hardcore gamers but it's a mainstream game :p)

--- Assets/Scripts/Environment/MovingPlatform.cs
+++ Assets/Scripts/Environment/MovingPlatform.cs
@@ -1,17 +1,17 @@
-﻿// https://www.youtube.com/watch?v=HMZnSZswTmU
+﻿using UnityEngine;
 
-using UnityEngine;
-using System.Collections;
+// https://www.youtube.com/watch?v=HMZnSZswTmU
 
 public class MovingPlatform : MonoBehaviour
 {
-
 	public GameObject platform;
-	public float moveSpeed;
-	public Transform[] points;
+    public float moveSpeed;
+    public Transform[] points;
 	public int pointSelection;
+    public float smoothVal = 4f;    // smoothVal = divisor coefficient
+    public float smoothDist = .8f;  // distance from which to slow down
 
-	Transform currentPoint;
+    Transform currentPoint;
 
 	// Use this for initialization
 	void Start ()
@@ -22,7 +22,25 @@ public class MovingPlatform : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		platform.transform.position = Vector3.MoveTowards (platform.transform.position, currentPoint.position, Time.deltaTime * moveSpeed);
+        float speed = moveSpeed;
+        if (smoothVal > 0f && smoothDist > 0f)
+        {
+            float distNext = Vector3.Distance(platform.transform.position, currentPoint.position);
+            // slow down when arriving to point
+            if (distNext < smoothDist)
+                speed = Mathf.Lerp(moveSpeed / smoothVal, moveSpeed, distNext / smoothDist);
+            // smoothly reaccelerate
+            else
+            {
+                int previous = (pointSelection - 1 < 0) ? points.Length - 1 : pointSelection - 1;
+                float distPrevious = Vector3.Distance(platform.transform.position, points[previous].position);
+                if (distPrevious< smoothDist)
+                    speed = Mathf.Lerp(moveSpeed / smoothVal, moveSpeed, distPrevious / smoothDist);
+            }
+            
+        }
+        platform.transform.position = Vector3.MoveTowards (platform.transform.position, currentPoint.position, Time.deltaTime * speed);
+
 		if (platform.transform.position == currentPoint.position) {
 			pointSelection++;
 			if (pointSelection >= points.Length)
