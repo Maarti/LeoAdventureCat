6e7368265edc0eda7aab3c50fd4c878e82c53680
Maarti <bryan.martinet@gmail.com>
2018-01-28 00:33:01 +0100

    1-story intro scene placeholder

---
+++ Tiled/Tiles/4_House/tileset_house.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset_house" tilewidth="128" tileheight="128" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="wood1.png"/>
+ </tile>
+</tileset>
