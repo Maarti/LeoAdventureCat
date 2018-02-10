eb6fe2713f29548dff9b2702b14c53cb3e21bf01
Maarti <bryan.martinet@gmail.com>
2018-02-10 20:32:28 +0100

    spike spinning self created, animated, scripted

---
+++ Assets/Scripts/Environment/SpikeSpinningShelf.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SpikeSpinningShelf : MonoBehaviour {
+
+    public float waitingTime = 3f;
+    public bool isSpikeDown = true;
+
+    float lastSpin;
+    Animator anim;
+    
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isSpikeDown", isSpikeDown);
+        lastSpin = Time.time;
+	}
+	
+	void Update () {
+        if (Time.time >= lastSpin + waitingTime)
+            Spin();
+	}
+
+    void Spin()
+    {
+        isSpikeDown = !isSpikeDown;
+        anim.SetBool("isSpikeDown", isSpikeDown);
+        lastSpin = Time.time;
+    }
+}
