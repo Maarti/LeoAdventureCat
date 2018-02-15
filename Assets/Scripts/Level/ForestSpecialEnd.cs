e024d0cb62af51f32bb1852f26b15c729abc1332
Maarti <bryan.martinet@gmail.com>
2018-02-15 20:34:58 +0100

    level 1-story reworked

--- Assets/Scripts/Level/ForestSpecialEnd.cs
+++ Assets/Scripts/Level/ForestSpecialEnd.cs
@@ -53,7 +53,7 @@ public class ForestSpecialEnd : EndSign
 
 		// Boss start absorbing
 		bossAnim.SetTrigger ("absorb");
-		yield return new WaitForSeconds (3f);
+		yield return new WaitForSeconds (2.5f);
 
 		// Cat "disapear"
 		SpriteRenderer[] playerSprites = player.GetComponentsInChildren<SpriteRenderer> ();
