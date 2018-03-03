e3490830a6c0e42991604727ef5da9e90a16e01d
Maarti <bryan.martinet@gmail.com>
2018-03-03 16:05:31 +0100

    lens flare added to city level

---
+++ Assets/Scripts/Level/City/CityFlareController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Flare))]
+public class CityFlareController : MonoBehaviour {
+    
+    public float localStart = 5f, localEnd = -5f;
+    public ObstaclesGenerator obsGen;
+    float distanceToTravel;
+    
+	void Start () {
+        distanceToTravel = obsGen.distanceToTravel;
+	}
+	
+	void Update () {
+        Vector3 localPos = this.transform.localPosition;
+        localPos.x = Mathf.Lerp(localStart, localEnd, transform.parent.position.x / distanceToTravel);
+        this.transform.localPosition = localPos;
+    }
+}
