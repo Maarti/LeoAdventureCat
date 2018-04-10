3d8d9bae68387fa705beb11979f86226b10075e7
Maarti <bryan.martinet@gmail.com>
2018-04-10 20:03:58 +0200

    created 'finish level' button in Inspector so I can cheat easily because i'm the developer

---
+++ Assets/Scripts/Editor/GameUIControllerEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameUIController))]
+public class GameUIControllerEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+        GameUIController gui = (GameUIController)target;
+        if (GUILayout.Button("Finish level"))
+            gui.EndGame();
+        if (GUILayout.Button("Game Over"))
+            gui.GameOver();
+    }
+}
